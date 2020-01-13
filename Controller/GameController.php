98eb34bf8545a8b3095176dc545a7a3b659c9aaf
Martin Pucan <martin.pucan@pilulka.cz>
2020-01-13 22:04:15 +0100

    sketch of czech game called suitcase

---
+++ Controller/GameController.php
@@ -0,0 +1,11 @@
+<?php 
+declare(strict_types=1);
+
+namespace Controller;
+
+class GameController {
+    public function process($parameters) 
+    {
+        $game = new GameBuild();
+    }
+}
---
+++ Model/GameBuild.php
@@ -0,0 +1,8 @@
+<?php
+declare(strict_types=1);
+
+namespace Model;
+
+class GameBuild {
+    public function 
+}
