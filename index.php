edcf489235ce2c84ea990ef0f1fd94e8f337186f
Martin Pucan <martin.pucan@pilulka.cz>
2020-06-01 00:52:42 +0200

    array functions

--- index.php
+++ index.php
@@ -2,8 +2,26 @@
 
 //phpinfo();
 
-echo 'Requested URL ="' . htmlspecialchars($_SERVER['QUERY_STRING']) . PHP_EOL . '"';
+//echo 'Requested URL ="' . htmlspecialchars($_SERVER['QUERY_STRING']) . PHP_EOL . '"';
 
-var_dump($_SERVER['QUERY_STRING']);
-var_dump($_SERVER['DOCUMENT_ROOT']);
-var_dump($_SERVER['SERVER_NAME']);
+//var_dump($_SERVER['QUERY_STRING']);
+//var_dump($_SERVER['DOCUMENT_ROOT']);
+//var_dump($_SERVER['SERVER_NAME']);
+
+
+$array = [
+    'else',
+    'non',
+    'good',
+    'great',
+    'great',
+    'non',
+];
+
+$result = array_unique($array);
+var_dump($result);
+
+$input = [100=>"red", 'yellow', 2=>"green", 3=>"blue", 4=>"yellow"];
+var_dump(array_slice($input, 0, 1)[0]);
+var_dump($input);
+var_dump(array_merge($input, [124, 125, 125,125,125, 12,5125]));
